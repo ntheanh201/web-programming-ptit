9b2dd4769c30035d16d0d570f9abf2c016ae941a
V_Dev <vuhoangviet160799@gmail.com>
2020-09-26 20:00:24 +0700

    client

---
+++ script.js
@@ -0,0 +1,47 @@
+
+function clickMenuIcon() {
+  var arr;
+  var menuSmall = document.getElementById("menu-small");
+  var arr = menuSmall.className.split(" ");
+  if (arr.indexOf("show") == -1) {
+    menuSmall.classList.add("show");
+  } else {
+    menuSmall.classList.remove("show");
+  }
+}
+
+window.onscroll = function () {
+  scrollFunc();
+};
+
+const scrollFunc = () => {
+  var h = window.scrollY;
+  // console.log('h : ', h);
+  var iconTop = document.getElementById("icon-top");
+  var header = document.getElementById("header");
+  if (h > 300) {
+    header.classList.add("fix-menu");
+  } else {
+    header.classList.remove("fix-menu");
+  }
+  if (h > 500) {
+    iconTop.classList.remove("hidden");
+  } else {
+    iconTop.classList.add("hidden");
+  }
+};
+
+function goTop() {
+  console.log("scroll");
+  window.scrollTo({
+    top: 0,
+  });
+}
+
+$(window).on('load', function(event) {
+  setTimeout(() => {
+    $('.preloader').addClass('hidden')
+  }, 1000);
+  // $('.preloader').delay(1000).fadeOut('fast');
+});
+
